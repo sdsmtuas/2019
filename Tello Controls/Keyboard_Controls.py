a9de6d7833724181f34d0b4201a3376293188aab
Justin Lewis <Justin.Diede@mines.sdsmt.edu>
2019-11-19 18:02:41 -0700

    Very basic outline of stuff There is only a main defined and called, with a while true loop created

--- Tello Controls/Keyboard_Controls.py
+++ Tello Controls/Keyboard_Controls.py
@@ -1,8 +1,16 @@
 """
-Author: Romain Caille and Justin Lewis
+Authors: Romain Caille
+         Justin Lewis
 Date: 11 / 19 / 2020
 """
 # Control drone with keyboard commands
 # Possibly implement joystick commands
 
 
+def main():
+    while True:
+        # if key:
+            # do something
+
+if __name__ == '__main__':
+    main()
